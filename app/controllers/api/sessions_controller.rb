37126cbc5c21b20a029a651b6aace02352148ee5
Evan <schwalm.evan@gmail.com>
2017-09-19 13:53:41 -0700

    create uesr and session controllers, frontend file structure

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render json: @user
+    else
+      render json: ['Invalid username or password'], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout!
+      render "/"
+    else
+      render json: ["User not logged in"], status: 404
+    end
+  end
+end
